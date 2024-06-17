d8c1a816e3051945b892e02d73a7d8f9eb678e63
ArtBclak <0960469634kvstrt@gmail.com>
2024-06-17 19:20:33 +0200

    js file to normal state 1

--- js/app.js
+++ js/app.js
@@ -6,64 +6,64 @@ document.addEventListener("DOMContentLoaded", () => {
 			navUl = document.querySelector(".nav_ul"),
 			navProgress = document.querySelector(".nav_progress");
 	// values
-	let model,
-		pX = 0,
-		pY = 1.4,
-		pZ = -1.1,
-		centerY = window.innerHeight / 2,
-		centerX = window.innerWidth / 2,
-		mouseX = 0, 
-		mouseY = 0;
+	let 	model,
+			load,
+			innerH = window.innerHeight,
+			innerW = window.innerWidth,
+			docHeight = document.documentElement.scrollHeight - innerH,
+			pX = 0,
+			pY = 1.4,
+			pZ = -1.1,
+			centerY = innerH / 2,
+			centerX = window.innerWidth / 2,
+			mouseX = 0, 
+			mouseY = 0;
 	// scene
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const 	scene = new THREE.Scene(),
+    	 	camera = new THREE.PerspectiveCamera( 100, innerW / innerH, 0.1, 1000 ),
+			renderer = new THREE.WebGLRenderer();
+
 	camera.position.set(pX, pY, pZ);
-	// camera.rotateY(1.57);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( innerW, innerH );
+
     document.body.appendChild( renderer.domElement );
 	// light
-	const ambientLight = new THREE.AmbientLight("wihte", .7);
-	scene.add(ambientLight);
+	const 	ambientLight = new THREE.AmbientLight("#ffffff", .7),
+			directionalLight = new THREE.DirectionalLight("gray", 10),
+			pointLight1 = new THREE.PointLight("blue", 1, 1000),
+			pointLight2 = new THREE.PointLight("yellow", 1, 1000);
 
-	// Додавання спрямованого світла
-	const directionalLight = new THREE.DirectionalLight("gray", 10);
 	directionalLight.position.set(5, 10, 7.5);
-	scene.add(directionalLight);
-
-	// Додавання точкового світла
-	const pointLight1 = new THREE.PointLight("yellow", 1, 10);
 	pointLight1.position.set(-3, 3, 3);
-	scene.add(pointLight1);
+	pointLight2.position.set(-3, 2, 3);
+	// light_add
+	scene.add(ambientLight, directionalLight, pointLight1, pointLight2);
 
-	const pointLight2 = new THREE.PointLight("yellow", 1, 10);
-	pointLight2.position.set(3, 3, 3);
-	scene.add(pointLight2);
 	// var
-	const loader = new THREE.GLTFLoader();
-	const modelUrl = './assets/model.glb';
+	const 	loader = new THREE.GLTFLoader(),
+			modelUrl = './assets/model.glb';
 
+	// remove_progress
 	const endLoading = () => {
 		loading.remove();
 		document.body.style.overflowY = "auto";
 		window.scrollTo(0, 0);
-	}
+	};
 
+	// model
 	loader.load( 
 		modelUrl, 
 		( gltf ) => {
 			model = gltf.scene
-			// model config
-			const box = new THREE.Box3().setFromObject(model);
-			const size = new THREE.Vector3();
+			const 	box = new THREE.Box3().setFromObject(model),
+			 		size = new THREE.Vector3();
 			model.position.set(0, 0, 0);
 			box.getSize(size);
-
 			scene.add( model );
-			endLoading()
+			endLoading();
 		},
 		(xhr) => {
-			let load = Math.round(xhr.loaded / xhr.total * 100)
+			load = Math.round(xhr.loaded / xhr.total * 100)
 			loadingTitle.textContent = `Loading ${load}` 
 		},
 		(error) => {
@@ -71,69 +71,61 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	);
 
-
-
-
-	function animate() {
+	// animate
+	const animate = () => {
 		requestAnimationFrame( animate );
 		renderer.render( scene, camera );
-	}
+	};
 	animate();
-	// window size
+
+	// window_size
 	window.addEventListener('resize', () => {
-		centerY = window.innerHeight / 2;
-		centerX = window.innerWidth / 2;
-		camera.aspect = window.innerWidth / window.innerHeight;
+		innerH = window.innerHeight;
+		innerW = window.innerWidth;
+		//
+		docHeight = document.documentElement.scrollHeight - innerH;
+		//
+		centerY = innerH / 2;
+		centerX = innerW / 2;
+		//
+		camera.aspect = innerW / innerH;
 		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.setSize(innerW, innerH);
 	});
 
 	// scroll
-
-
 	const scroll = () => {
-		let docHeight = document.documentElement.scrollHeight - window.innerHeight;
 		let h = window.scrollY / docHeight;
-	
 		navProgress.style.width = h*100+"%";
 		camera.position.set(pX, pY+h/4, pZ+h);
-
 	};
+	window.addEventListener("scroll", scroll);
 
-	window.addEventListener("scroll", () => {
-		scroll();
-	});
+	// mouse 
+	const onMouseMove = (e) => {
+		mouseX = e.clientX - centerX;
+		mouseY = centerY - e.clientY; 
+		if (model) {
+			model.rotation.y = mouseX/8000
+			model.rotation.z = mouseY/8000
+		}
+	};
+	window.addEventListener('mousemove', onMouseMove);
 
-
-
-
-
-
-
+	// navigation 
 	navUl.addEventListener('click', (event) => {
 		const target = event.target;
 		if (target.classList.contains('nav_li')) {
-			const childrenArray = Array.from(navUl.children);
-			const index = childrenArray.indexOf(target);
-			const scrollHeight = document.documentElement.scrollHeight;
-            const clientHeight = window.innerHeight;
+			const 	childrenArray = Array.from(navUl.children),
+					index = childrenArray.indexOf(target),
+			 		scrollHeight = document.documentElement.scrollHeight;
 
 			window.scrollTo({
-                top: (scrollHeight - clientHeight) * (1/childrenArray.length * index),
+                top: (scrollHeight - innerH) * (1/childrenArray.length * index),
                 behavior: 'smooth'
             });
 		};
 	});
-
-	function onMouseMove(event) {
-		mouseX = event.clientX - centerX;
-		mouseY = centerY - event.clientY; 
-		model.rotation.y = mouseX/8000
-		model.rotation.z = mouseY/8000
-	}
-
-	window.addEventListener('mousemove', onMouseMove, false);
-
 });
 
 
