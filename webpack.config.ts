7b8f3c6c0317252511d4ebcb0e44661889a124f9
Artem Zaikin <0960469634kvstrt@gmail.com>
2024-07-15 12:45:13 +0200

    dist

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,16 +6,14 @@ import { BuildMode } from './config/types/buildTypes';
 interface EnvVariables {
     mode: BuildMode;
     port: number;
-}
-
-
+};
 
 export default (env: EnvVariables) => {
     const paths = {
         entry: path.resolve(__dirname, "src", "index.ts"),
         html: path.resolve(__dirname, "src", "pages"),
         outputDev: path.resolve(__dirname, "build"),
-        outputProd: path.resolve(__dirname, "./"),
+        outputProd: path.resolve(__dirname, "dist"),
     }
     const config: webpack.Configuration = buildWebpack({
         port: env.port ?? 3000,
@@ -23,4 +21,4 @@ export default (env: EnvVariables) => {
         paths
     })
     return config;
-}
+};
