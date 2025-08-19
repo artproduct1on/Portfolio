7709d92026c0aa03ed8b5948e79db1dd2c1ddc4b
Artem <artem.99.zaikin@gmail.com>
2025-08-19 21:01:37 +0200

    about add new info

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -25,12 +25,10 @@ export default async function AboutPage() {
 
   const t = await getTranslations("about");
 
-  const list = t.raw("list");
-
   return <>
-    <AnimatedSection className={s.section} id="about">
+    <AnimatedSection className={s.hero} id="about">
       <h1 className={s.title}>
-        {t("title")}
+        {t("hero.title")}
       </h1>
 
       <div className={s.block}>
@@ -38,18 +36,18 @@ export default async function AboutPage() {
           <Image
             className={s.photoImg}
             src="/me.png"
-            alt={t("photo")}
+            alt={t("hero.photo")}
             loading="lazy"
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
           <p className={s.photoText}>
-            {t("photo")}
+            {t("hero.photo")}
           </p>
         </div>
 
         <ul className={s.list}>
-          {list.map(([title, desc]: [string, string], i: number) => (
+          {t.raw("hero.list").map(([title, desc]: [string, string], i: number) => (
             <li className={s.listLi} key={i}>
               <span className={s.listSpan}>
                 {title}
@@ -61,10 +59,34 @@ export default async function AboutPage() {
           ))}
         </ul>
       </div>
+    </AnimatedSection>
+    <AnimatedSection className={s.career} id="career">
+      <h2>
+        {t("career.h2")}
+      </h2>
       <h3>
-        {t("h3")}
+        {t("career.h3")}
       </h3>
       <Clock start={startCareer} />
+      <p className={s.careerText}>
+        {t("career.info")}
+      </p>
+      <h3>
+        {t("career.info_subtitle")}
+      </h3>
+      <ul className={s.careerList}>
+        {t.raw("career.info_list").map((i: string, index: number) => (
+          <li
+            key={index}
+            className={s.careerItem}
+          >
+            <span className={s.careerItemSpan}>
+              0{index + 1}
+            </span>
+            {i}
+          </li>
+        ))}
+      </ul>
     </AnimatedSection>
   </>;
 }
